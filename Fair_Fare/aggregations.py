dc3979bc6c7abbb1dcef6b3a28de05182edf2657
Isaac K <isaac.khader@gmail.com>
2019-07-15 14:11:30 -0600

    Adding a class that builds everything we need for fare prediction from lat, long and user input. Re-running ML models with reduced feature set.

--- Fair_Fare/aggregations.py
+++ Fair_Fare/aggregations.py
@@ -11,34 +11,30 @@ class agg_on_trips(object):
     (lat, long) coordinates."""
     def __init__(self, dataframe):
         self.df = dataframe
-        self.n_bins = 50
-
-
-    def rename_ll(self):
-        """to make lat and long easier to type"""
-        name_mapping = {'Pickup_Centroid_Latitude': 'latp',
-                        'Dropoff_Centroid_Latitude': 'latd',
-                        'Pickup_Centroid_Longitude': 'longp',
-                        'Dropoff_Centroid_Longitude': 'longd'}
-        self.df.rename(index=str, columns = name_mapping, inplace=True)
+        self.geo_cols = ['Pickup_Centroid_Latitude','Dropoff_Centroid_Latitude','Pickup_Centroid_Longitude','Dropoff_Centroid_Longitude']
+        self.n_bins = 20
+        self.total_unique_paths = None
 
 
     def find_boundary(self, padding = 0.001):
-        topleft = (max(self.df.latp.max(),self.df.latd.max())+padding,min(self.df.longp.min(),self.df.longd.min())-padding)
-        bottomright = (min(self.df.latp.min(),self.df.latd.min())-padding,max(self.df.longp.max(),self.df.longd.max())+padding)
+        topleft = (max(self.df.Pickup_Centroid_Latitude.max(),self.df.Dropoff_Centroid_Latitude.max())+padding,
+                   min(self.df.Pickup_Centroid_Longitude.min(),self.df.Dropoff_Centroid_Longitude.min())-padding)
+        bottomright = (min(self.df.Pickup_Centroid_Latitude.min(),self.df.Dropoff_Centroid_Latitude.min())-padding,
+                       max(self.df.Pickup_Centroid_Longitude.max(),self.df.Dropoff_Centroid_Longitude.max())+padding)
         return topleft, bottomright
 
     def coord_bins(self):
         """Generate the grid from the lat and long."""
         # TODO: consider non-rectangular grid
-        topleft, bottomright = self.find_boundary(self.df)
-        lat_axis = np.linspace(bottomright[0],topleft[0],n_bins)
-        long_axis = np.linspace(topleft[1],bottomright[1],n_bins)
+        topleft, bottomright = self.find_boundary()
+        lat_axis = np.linspace(bottomright[0],topleft[0],self.n_bins)
+        long_axis = np.linspace(topleft[1],bottomright[1],self.n_bins)
         return lat_axis, long_axis
     
 
-    def cut_along_lat_long(self, locs):
+    def cut_along_lat_long(self):
         """Add a _b binned column foreach lat and long, also a tuple of all the bins"""
+        locs = self.geo_cols
         labels = np.arange(self.n_bins-1)
         new_colname = {loc:loc + "_b" for loc in locs}
         lat_axis, long_axis = self.coord_bins()
@@ -49,5 +45,10 @@ class agg_on_trips(object):
         for long in locs[2:]:
             self.df[new_colname[long]] = pd.cut(self.df[long], bins = long_axis,labels = labels, retbins=False)
             
-        self.df['path'] = list(zip(zip(self.df.latp_b,self.df.longp_b), zip(self.df.latd_b,self.df.longd_b)))
+        # path is a list 
+        self.df['path'] = list(zip(
+            zip(self.df.Pickup_Centroid_Latitude_b,self.df.Pickup_Centroid_Longitude_b),
+            zip(self.df.Dropoff_Centroid_Latitude_b,self.df.Dropoff_Centroid_Longitude_b))
+                              )
+        self.total_unique_paths = self.df.groupby('path').Trip_Miles.count().count()
         
